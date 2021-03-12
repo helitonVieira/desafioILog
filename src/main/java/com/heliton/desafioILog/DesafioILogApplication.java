ff67eb859627ef94ab0e40207fa9e67ba2cc3173
heliton Vieira <helitondba@gmail.com>
2021-03-12 13:50:26 -0300

    Primeiro commit e criação da Class Funcionario

---
+++ src/main/java/com/heliton/desafioILog/DesafioILogApplication.java
@@ -0,0 +1,13 @@
+package com.heliton.desafioILog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioILogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioILogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliton/desafioILog/DesafioILogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliton.desafioILog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioILogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
